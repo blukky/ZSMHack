c107d1734e1371c3013513034694603e9a801a17
Dima <ya.gud@mail.ru>
2021-04-24 12:50:45 +0300

    сломал реформ продукт

--- wallet one.py
+++ wallet one.py
@@ -1,30 +1,8 @@
 import requests as r
 import datetime
-from collections import defaultdict
-import binascii
-from hashlib import md5
+
 
 URL = 'https://api.dev.walletone.com/p2p'
-
-
-def get_signature(params, secret_key):
-    """
-    Base64(Byte(MD5(Windows1251(Sort(Params) + SecretKey))))
-    params - list of tuples [('WMI_CURRENCY_ID', 643), ('WMI_PAYMENT_AMOUNT', 10)]
-    """
-    icase_key = lambda s: str(s, 'utf-8').lower()
-
-    lists_by_keys = defaultdict(list)
-    for key, value in params:
-        lists_by_keys[key].append(value)
-
-    str_buff = ''
-    for key in sorted(lists_by_keys, key=icase_key):
-        for value in sorted(lists_by_keys[key], key=icase_key):
-            str_buff += str(value, 'utf-8').encode('1251')
-    str_buff += secret_key
-    md5_string = md5(str_buff).digest()
-    return binascii.b2a_base64(md5_string)[:-1]
 
 
 time = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
@@ -47,6 +25,3 @@ body = {
 }
 
 
-
-answer = r.post(url=URL + '/api/v3/deals', data=body)
-print(answer)
